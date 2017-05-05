57951d85efa17a1a8873bfdd3bdc8ce8c5e265f7
HenryLiu <583448542@qq.com>
2017-05-05 16:15:23 +0800

    test cmd

--- detail.js
+++ detail.js
@@ -102,6 +102,17 @@ router.route('/')
         inference = [[5,1],[1,0],[4,0]];
     }
     else{
+        var exec = require('child_process').exec; 
+        var cmdStr = 'curl http://www.weather.com.cn/data/sk/101010100.html';
+        exec(cmdStr, function(err,stdout,stderr){
+            if(err) {
+                console.log('get weather api error:'+stderr);
+            } else {
+                var data = JSON.parse(stdout);
+                console.log(data);
+            }
+        });
+
         filename = 'les-miserables.gexf';
         var data = {
             head : '',
